deb80329b575123bb55cd7c83ab427baee71593c
Chris Wiggins <chris@chriswiggins.co>
2015-04-10 14:37:50 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp'),
+	size = require('gulp-size'),
+	minifyCSS = require('gulp-minify-css'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	sass = require('gulp-sass'),
+	watch = require('gulp-watch'),
+	browserSync = require('browser-sync');
+
+// Styles
+gulp.task('styles', function () {
+    return gulp.src('./sass/wgzio.scss')
+        .pipe(sass({errLogToConsole: true}))
+        .pipe(size({gzip: true, showFiles: true}))
+        .pipe(gulp.dest('./css/'))
+        .pipe(minifyCSS())
+        .pipe(rename('wgzio.min.css'))
+        .pipe(gulp.dest('./css/'))
+});
+
+// Process lib javascript files
+gulp.task('vendor', function() {  
+    return gulp.src('js/lib/*.js')
+        .pipe(concat('vendor.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('js/min/vendor.js'))
+});
+
+// Starts a server using Browsersync
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+// Reloads server
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+
+gulp.task('default', ['styles', 'browser-sync', 'bs-reload'], function() {
+	gulp.start('styles');
+	gulp.watch('sass/*.scss', ['styles','bs-reload']);
+	gulp.watch('*.html', ['bs-reload']);
+});
